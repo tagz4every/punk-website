43bdb38c5e727bdfe747ae4c0ff5549dbc462afd
Tagz <tagz4everyone@gmail.com>
2020-04-03 20:05:38 -0300

    Header fully designed

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'; //imr
+import Header from './containers/header/Header'
+
+const App = () => { //sfc
+    return ( 
+        <Header/>
+     );
+}
+ 
+export default App;
---
+++ src/containers/header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import "./Header.sass";
+
+const Header = () => {
+    return ( 
+        <header>
+            <div className="background-color"></div>
+            <div className="warning-top">
+                <div className="top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="warning-bottom">
+                <div className="top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="pcloop-top"></div>
+            <div className="pcloop-bottom"></div>
+            <div className="grid">
+                <div className="gato">
+                    <img src={require('../../assets/cat.png')} alt=""/>
+                </div>
+                <div className="logo"><img src={require('../../assets/logo.png')} alt=""/></div>
+            </div>
+            <div className="background-image"></div>
+        </header>
+    );
+}
+ 
+export default Header;
